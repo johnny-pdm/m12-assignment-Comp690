83833f282156329ddacb2f7fcae1002803abf41f
unknown <j.rivera@powerdigitalmarketing.com>
2024-06-06 13:43:06 -0400

    Updated player

--- js/player.js
+++ js/player.js
@@ -3,7 +3,19 @@ class Jukebox {
         this.albums = []
     }
     addAlbum = function(album) {
-        this.albums.push(album)
+        this.albums.push(album);
+        this.createDropdown();
+    }
+    createDropdown = function() {
+        let _select = document.querySelector('#albums');
+        let _options = "";
+        this.albums.forEach(album => {
+            let albumID = album.title.toLowerCase().split(" ").join("_");
+            _options += `<option id="${ albumID }" >${ album.title }</option>`;
+        });
+
+        console.log(_options);
+        _select.innerHTML = _options;
     }
     favoriteAlbum = function() {
         let max = -1, fav
@@ -31,21 +43,49 @@ class Album {
     }
 }
 
-var jbox = new Jukebox()
-const album1 = new Album('Operation Ivy', 'Energy')
-const album2 = new Album('Blink 182', 'Dude Ranch')
-const album3 = new Album('New Found Glory', 'Sticks and Stones')
+let albumsArr = [
+    {
+        artist: 'Operation Ivy',
+        album: 'Energy'
+    },
+    {
+        artist: 'Blink 182',
+        album: 'Dude Ranch'
+    },
+    {
+        artist: 'New Found Glory',
+        album: 'Sticks and Stones'
+    }
+];
 
-jbox.addAlbum(album1)
-jbox.addAlbum(album2)
-jbox.addAlbum(album3)
+let albums = []
+var jbox = new Jukebox();
 
-album1.play()
-album2.play()
-album2.play()
-album2.play()
-album2.play()
-album2.play()
-album3.play()
+for(let i = 0; i < albumsArr.length; i++) {
+    albums[i] = new Album(albumsArr[i]['artist'], albumsArr[i]['album']);
+    jbox.addAlbum(albums[i]);
+}
 
-console.log(`Your favorite album is: ${jbox.favoriteAlbum()}`)
+// const album1 = new Album('Operation Ivy', 'Energy')
+// const album2 = new Album('Blink 182', 'Dude Ranch')
+// const album3 = new Album('New Found Glory', 'Sticks and Stones')
+
+// jbox.addAlbum(album1)
+// jbox.addAlbum(album2)
+// jbox.addAlbum(album3)
+
+document.querySelector("#play").addEventListener('click', function(e){
+    let albumIndex = document.querySelector('#albums').selectedIndex;
+    albums[albumIndex].play();
+    document.querySelector('#favoriteAlbum').innerText = `Your favorite album is: ${jbox.favoriteAlbum()}`
+});
+
+// album1.play()
+// album2.play()
+// album2.play()
+// album2.play()
+// album2.play()
+// album2.play()
+// album3.play()
+
+document.querySelector('#favoriteAlbum').innerText = `Your favorite album is: ${jbox.favoriteAlbum()}`
